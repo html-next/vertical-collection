701ec971644883228b28f136cc8c1c08d0c90295
Chris Thoburn <cthoburn@linkedin.com>
2017-04-28 01:01:06 -0700

    simplify scroll update

--- addon/-private/data-view/radar/radar.js
+++ addon/-private/data-view/radar/radar.js
@@ -289,21 +289,6 @@ export default class Radar {
     }
   }
 
-  _updatePoolContents() {
-    const {
-      orderedComponents,
-      firstItemIndex,
-      items
-    } = this;
-
-    for (let i = 0; i < orderedComponents.length; i++) {
-      let component = orderedComponents[i];
-      let newContent = objectAt(items, firstItemIndex + i);
-
-      set(component, 'content', newContent);
-    }
-  }
-
   prepend(items, numPrepended) {
     this.items = items;
     this.firstItemIndex += numPrepended;
@@ -329,9 +314,8 @@ export default class Radar {
       this.firstItemIndex = null;
       this.lastItemIndex = null;
       this._updateVirtualComponentPool();
-      this.scheduleUpdate();
-    } else {
-      this._updatePoolContents();
     }
+
+    this.scheduleUpdate();
   }
 }
