845aebffb058396a815f78ec4a7b8b33bb4e768b
Chris Thoburn <cthoburn@linkedin.com>
2017-04-28 22:13:23 -0700

    fix some flaky async stuff

--- config/ember-try.js
+++ config/ember-try.js
@@ -6,15 +6,18 @@ module.exports = {
       bower: {
         dependencies: {
           'ember': '~1.11.0',
+          'ember-cli-shims': 'ember-cli/ember-cli-shims#0.0.3',
           'ember-data': '1.0.0-beta.16.1'
         },
         resolutions: {
           'ember': '~1.11.0',
+          'ember-cli-shims': '0.0.3',
           'ember-data': '1.0.0-beta.16.1'
         }
       },
       npm: {
         devDependencies: {
+          'ember-cli-shims': null,
           'ember-data': "1.0.0-beta.16.1",
           'ember-source': null
         }
@@ -25,15 +28,18 @@ module.exports = {
       bower: {
         dependencies: {
           'ember': '~1.13.0',
+          'ember-cli-shims': '0.0.6',
           'ember-data': '~1.13.0'
         },
         resolutions: {
           'ember': '~1.13.0',
+          'ember-cli-shims': '0.0.6',
           'ember-data': '~1.13.0'
         }
       },
       npm: {
         devDependencies: {
+          'ember-cli-shims': null,
           'ember-data': "~1.13.15",
           'ember-source': null
         }
