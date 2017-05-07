0e0f26645ca7e3ef2bc095a1168129cabc5bb657
Chris Thoburn <runspired@users.noreply.github.com>
2017-05-06 22:58:12 -0700

    yay! :) (#80)
    * yay! :)
    
    * fix rsvp for older Ember

--- tests/dummy/app/adapters/application.js
+++ tests/dummy/app/adapters/application.js
@@ -1,3 +1,26 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 
-export default DS.RESTAdapter.extend();
+const {
+  RSVP
+} = Ember;
+
+const NUMBERS = {
+  data: []
+};
+
+for (let i = 0; i < 100; i++) {
+  NUMBERS.data.push({
+    type: 'number-item',
+    id: `${i}`,
+    attributes: {
+      number: i
+    }
+  });
+}
+
+export default DS.JSONAPIAdapter.extend({
+  findAll() {
+    return RSVP.Promise.resolve(NUMBERS);
+  }
+});
--- tests/dummy/mirage/serializers/application.js
+++
@@ -1,4 +0,0 @@
-// mirage/serializers/application.js
-import { RestSerializer } from 'ember-cli-mirage';
-
-export default RestSerializer;
