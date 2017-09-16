31df697ee1344afa3a8c08fcf2cf2f82d0b36ed3
Christopher Garrett <me@pzuraq.com>
2017-09-16 13:57:11 -0700

    cleanup(helpers): Removes unused helper functions

--- addon/-private/utils/element/apply-dimensions.js
+++
@@ -1,9 +0,0 @@
-// TODO is this util used
-export default function applyDimensions(element, dimensions) {
-  for (const i in dimensions.style) {
-    // TODO add explicit test
-    if (dimensions.style.hasOwnProperty(i)) {
-      element.style[i] = i === 'boxSizing' ? dimensions.style[i] : `${dimensions.style[i]}px`;
-    }
-  }
-}
