8bc3cce295c63f846fceb33b487529ab24b0e226
Robert Wagner <rwwagner90@gmail.com>
2018-01-18 12:25:26 -0500

    Update test-helper.js

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -1,8 +1,12 @@
+import 'vertical-collection/-debug';
+import registerWaiter from 'ember-raf-scheduler/test-support/register-waiter';
 import Application from '../app';
 import config from '../config/environment';
 import { setApplication } from '@ember/test-helpers';
 import { start } from 'ember-qunit';
 
 setApplication(Application.create(config.APP));
+
+registerWaiter();
 
 start();
