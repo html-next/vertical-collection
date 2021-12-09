be1d2ab00dfcdee9e320d8b05113655b3846fee0
Matthew Beale <matt.beale@madhatted.com>
2021-12-09 16:30:00 -0500

    4 retries on CI, 1s sleep

--- bin/run-tests-with-retry.sh
+++ bin/run-tests-with-retry.sh
@@ -4,7 +4,7 @@ function retry {
     command="$*"
     retval=1
     attempt=1
-    until [[ $retval -eq 0 ]] || [[ $attempt -gt 3 ]]; do
+    until [[ $retval -eq 0 ]] || [[ $attempt -gt 4 ]]; do
         # Execute inside of a subshell in case parent
         # script is running with "set -e"
         (
@@ -15,10 +15,10 @@ function retry {
         attempt=$(( $attempt + 1 ))
         if [[ $retval -ne 0 ]]; then
             # If there was an error wait 10 seconds
-            sleep 10
+            sleep 1
         fi
     done
-    if [[ $retval -ne 0 ]] && [[ $attempt -gt 3 ]]; then
+    if [[ $retval -ne 0 ]] && [[ $attempt -gt 4 ]]; then
         # Something is fubar, go ahead and exit
         exit $retval
     fi
