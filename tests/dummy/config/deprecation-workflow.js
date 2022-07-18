ea99cb6c06e774aacd944d5333a52fd43a372a42
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-07-18 13:57:43 -0400

    Use ember 4 compatible versions of test deps

--- tests/dummy/config/deprecation-workflow.js
+++ tests/dummy/config/deprecation-workflow.js
@@ -1,8 +1,9 @@
+/* eslint-disable no-undef */
 self.deprecationWorkflow = self.deprecationWorkflow || {};
 self.deprecationWorkflow.config = {
   throwOnUnhandled: true,
   workflow: [
     // Resolving this requires updating all the tests
-    { handler: "silence", matchId: "ember-test-helpers.setup-rendering-context.render" }
+    { handler: 'silence', matchId: 'ember-test-helpers.setup-rendering-context.render' }
   ]
 };
