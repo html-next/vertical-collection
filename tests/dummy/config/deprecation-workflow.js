7590d41cef6c949bbdd4a72bd94121ede36fc3aa
NullVoxPopuli <LPSego3+dev@gmail.com>
2022-07-19 22:40:21 -0400

    don't default to throw exceptions on every deprecation

--- tests/dummy/config/deprecation-workflow.js
+++ tests/dummy/config/deprecation-workflow.js
@@ -1,7 +1,7 @@
 /* eslint-disable no-undef */
 self.deprecationWorkflow = self.deprecationWorkflow || {};
 self.deprecationWorkflow.config = {
-  throwOnUnhandled: true,
+  throwOnUnhandled: false,
   workflow: [
     // Resolving this requires updating all the tests
     { handler: 'silence', matchId: 'ember-test-helpers.setup-rendering-context.render' }
