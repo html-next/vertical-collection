bf57185bfbdf64aaec2d0c03f3318d3e6f4d2ab8
Matthew Beale <matt.beale@madhatted.com>
2022-07-19 20:57:40 -0400

    Drop Ember versions prior to 3.12 from CI

--- config/ember-try.js
+++ config/ember-try.js
@@ -7,37 +7,6 @@ module.exports = async function() {
     useYarn: true,
     scenarios: [
       {
-        name: 'ember-lts-2.18',
-        npm: {
-          devDependencies: {
-            '@ember/jquery': '^1.1.0',
-            '@ember/test-helpers': '^1.7.0',
-            'ember-angle-bracket-invocation-polyfill': '^3.0.1',
-            'ember-qunit': '^4.0.0',
-            'ember-source': '~2.18.0',
-            'qunit': null
-          },
-        },
-      },
-      {
-        name: 'ember-lts-3.4',
-        npm: {
-          devDependencies: {
-            'ember-angle-bracket-invocation-polyfill': '^3.0.1',
-            'ember-source': '~3.4.0'
-          }
-        }
-      },
-      {
-        name: 'ember-lts-3.8',
-        npm: {
-          devDependencies: {
-            'ember-angle-bracket-invocation-polyfill': '^3.0.1',
-            'ember-source': '~3.8.0'
-          }
-        }
-      },
-      {
         name: 'ember-lts-3.12',
         npm: {
           devDependencies: {
@@ -63,15 +32,6 @@ module.exports = async function() {
         }
       },
       {
-        name: 'ember-lts-3.24',
-        npm: {
-          devDependencies: {
-            'ember-data': '~3.24.0',
-            'ember-source': '~3.24.0'
-          }
-        }
-      },
-      {
         name: 'ember-lts-3.28',
         npm: {
           devDependencies: {
@@ -87,25 +47,6 @@ module.exports = async function() {
             'ember-cli-fastboot': '3.2.0-beta.5',
             'ember-data': '~3.28.0',
             'ember-source': '~4.4.0'
-          },
-          ember: {
-            edition: 'octane'
-          }
-        },
-        env: {
-          EMBER_OPTIONAL_FEATURES: JSON.stringify({
-            'application-template-wrapper': false,
-            'template-only-glimmer-components': true,
-          })
-        }
-      },
-      {
-        name: 'ember-4.5',
-        npm: {
-          devDependencies: {
-            'ember-cli-fastboot': '3.2.0-beta.5',
-            'ember-data': '~3.28.0',
-            'ember-source': '~4.5.0'
           },
           ember: {
             edition: 'octane'
