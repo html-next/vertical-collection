4eaa82e75f645d224c6011a8a4d1e973323355a8
Matthew Beale <matt.beale@madhatted.com>
2022-08-26 17:29:14 -0400

    Add QUnit test timeout

--- tests/test-helper.js
+++ tests/test-helper.js
@@ -3,7 +3,10 @@ import registerWaiter from 'ember-raf-scheduler/test-support/register-waiter';
 import Application from '../app';
 import config from '../config/environment';
 import { setApplication } from '@ember/test-helpers';
+import QUnit from 'qunit';
 import { start } from 'ember-qunit';
+
+QUnit.config.testTimeout = 5000;
 
 setApplication(Application.create(config.APP));
 
