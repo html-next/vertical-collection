5f1f6c38341b4d6ab8e2263d187e9adf3908a08b
ignace <ignace@otainsight.com>
2023-09-11 21:15:43 +0200

    feat: use ember-shiki in dummy app

--- tests/dummy/app/components/code-snippet.js
+++ tests/dummy/app/components/code-snippet.js
@@ -3,6 +3,14 @@ import snippets from '../snippets';
 
 export default class extends Component {
   get snippet() {
-    return snippets[this.args.name]?.content;
+    const snippet = snippets[this.args.name]?.content.split('\n');
+    const indentAmount = snippet[0].length - snippet[0].trimStart().length;
+
+    return snippet.map((line) => line.substring(indentAmount)).join('\n');
+  }
+
+  get language() {
+    const filenameParts = snippets[this.args.name]?.source.split('.');
+    return filenameParts[filenameParts.length - 1];
   }
 }
