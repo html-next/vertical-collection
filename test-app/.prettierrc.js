73780b54d293bacc67da1fd185855ee82c5dfb72
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-05-16 11:44:33 -0400

    ope

--- test-app/.prettierrc.js
+++ test-app/.prettierrc.js
@@ -1,6 +1,7 @@
 'use strict';
 
 module.exports = {
+  printWidth: 120,
   overrides: [
     {
       files: '*.{js,ts}',
--- test-app/app/components/number-slide.js
+++ test-app/app/components/number-slide.js
@@ -2,7 +2,6 @@ import { alias } from '@ember/object/computed';
 import Component from '@ember/component';
 import { computed } from '@ember/object';
 import { htmlSafe } from '@ember/template';
-import layout from './template';
 
 function numberToOpacity(number) {
   let r = number % 255;
@@ -22,14 +21,11 @@ export default Component.extend({
   attributeBindings: ['style'],
   isDynamic: false,
   prefixed: false,
-  style: computed('isDynamic', 'item', function() {
+  style: computed('isDynamic', 'item', function () {
     let item = this.item;
     let isDynamic = this.isDynamic;
 
-    let {
-      height,
-      number
-    } = item;
+    let { height, number } = item;
 
     let opacity = numberToOpacity(number);
     let styleStr = `background: rgba(0,125,255,${opacity});`;
@@ -40,8 +36,7 @@ export default Component.extend({
 
     return htmlSafe(styleStr);
   }),
-  layout,
   itemIndex: 0,
   item: null,
-  number: alias('item.number')
+  number: alias('item.number'),
 });
