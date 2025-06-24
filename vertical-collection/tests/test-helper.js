8d261a410ecd491eb8751a3badd91322bed459ca
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-06-23 21:09:05 -0400

    Revert unneeded change

--- vertical-collection/tests/test-helper.js
+++ vertical-collection/tests/test-helper.js
@@ -4,7 +4,6 @@ import config from '../config/environment';
 import { setApplication } from '@ember/test-helpers';
 import QUnit from 'qunit';
 import { start } from 'ember-qunit';
-import { loadTests } from 'ember-qunit/load-tests';
 
 QUnit.config.testTimeout = 5000;
 
@@ -12,5 +11,4 @@ setApplication(Application.create(config.APP));
 
 registerWaiter();
 
-loadTests();
 start();
