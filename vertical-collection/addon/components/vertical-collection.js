932fc47ad88ea5f735d6ed22d0b54309cf040a47
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-07-10 12:06:04 -0400

    Use co-located components instead of pods (#444)
    * Use co-located components instead of pods
    
    * Update deps
    
    * undo dep changes
    
    * Revert unneeded change
    
    * Adjust path

--- vertical-collection/addon/components/vertical-collection.js
+++ vertical-collection/addon/components/vertical-collection.js
@@ -15,7 +15,6 @@ import { empty, readOnly } from '@ember/object/computed';
 import Component from '@ember/component';
 import { get, computed } from '@ember/object';
 import { run } from '@ember/runloop';
-import layout from './template';
 
 import { scheduler, Token } from 'ember-raf-scheduler';
 
@@ -25,7 +24,7 @@ import {
   DynamicRadar,
   StaticRadar,
   objectAt,
-} from '../../-private/index';
+} from '../-private/index';
 
 /*
  * BEGIN DEBUG HELPERS
@@ -197,8 +196,6 @@ class Visualization {
  */
 
 const VerticalCollection = Component.extend({
-  layout,
-
   tagName: '',
 
   /**
