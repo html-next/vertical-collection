6e77bd5f7b0f6153ee39d0b3519ad424c178165b
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-11-26 13:17:23 -0500

    Add warp-drives config

--- test-app/ember-cli-build.js
+++ test-app/ember-cli-build.js
@@ -7,7 +7,19 @@ const { compatBuild } = require('@embroider/compat');
 module.exports = async function (defaults) {
   const { buildOnce } = await import('@embroider/vite');
 
+  const { setConfig } = await import('@warp-drive/core/build-config');
+
   let app = new EmberApp(defaults, {});
+
+  setConfig(app, __dirname, {
+    // this should be the most recent <major>.<minor> version for
+    // which all deprecations have been fully resolved
+    // and should be updated when that changes
+    compatWith: '4.12',
+    deprecations: {
+      // ... list individual deprecations that have been resolved here
+    },
+  });
 
   return compatBuild(app, buildOnce);
 };
