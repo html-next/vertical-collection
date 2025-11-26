322b44b12703876657c3c303829465c42e8bea5f
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2025-11-26 15:50:17 -0500

    Handle some deprecations

--- test-app/ember-cli-build.js
+++ test-app/ember-cli-build.js
@@ -15,9 +15,8 @@ module.exports = async function (defaults) {
     // this should be the most recent <major>.<minor> version for
     // which all deprecations have been fully resolved
     // and should be updated when that changes
-    compatWith: '4.12',
     deprecations: {
-      // ... list individual deprecations that have been resolved here
+      DEPRECATE_STORE_EXTENDS_EMBER_OBJECT: false,
     },
   });
 
