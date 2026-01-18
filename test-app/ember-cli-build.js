fc8680765f312dc0649fa68e48d3347743df72d5
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2026-01-18 10:56:06 -0500

    Add Bootstrap and update loading spinner styles
    - Imported Bootstrap CSS and glyphicons for styling support.
    - Updated loading spinner background to use SVG and adjusted size.
    - Added Bootstrap as a dependency in package.json.

--- test-app/ember-cli-build.js
+++ test-app/ember-cli-build.js
@@ -11,6 +11,26 @@ module.exports = async function (defaults) {
 
   let app = new EmberApp(defaults, {});
 
+  // The v1 dummy app imported Bootstrap (and glyphicons fonts) to support
+  // the demo templates' grid/table styling.
+  let bootstrapPath = 'node_modules/bootstrap/dist/';
+  app.import(`${bootstrapPath}css/bootstrap.css`);
+  app.import(`${bootstrapPath}fonts/glyphicons-halflings-regular.eot`, {
+    destDir: 'fonts',
+  });
+  app.import(`${bootstrapPath}fonts/glyphicons-halflings-regular.svg`, {
+    destDir: 'fonts',
+  });
+  app.import(`${bootstrapPath}fonts/glyphicons-halflings-regular.ttf`, {
+    destDir: 'fonts',
+  });
+  app.import(`${bootstrapPath}fonts/glyphicons-halflings-regular.woff`, {
+    destDir: 'fonts',
+  });
+  app.import(`${bootstrapPath}fonts/glyphicons-halflings-regular.woff2`, {
+    destDir: 'fonts',
+  });
+
   setConfig(app, __dirname, {
     // this should be the most recent <major>.<minor> version for
     // which all deprecations have been fully resolved
